37a8edebb402b173b543550542f248ad4ce0609d
Jakub Maguza <jakub.maguza@gmail.com>
2017-05-19 20:47:27 +0200

    AllegroHelper and AllegroItem classes...
    AllegroItem class maps items from RSS Feed
    AllegroHelper contain method to conver XML from RSS to AllegroItem

---
+++ src/AllegroHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace AllegroLINQ.src
+{
+    static class AllegroHelper
+    {
+        public static IEnumerable<AllegroItem> TryParseAllegroXml(XElement xel)
+        {
+            var channelElements = xel.Elements().First().Elements("item");
+            foreach (var employee in channelElements)
+            {
+                AllegroItem ai = new AllegroItem();
+                ai.Title = employee.Element("title").Value;
+                ai.ItemUri = new Uri(employee.Element("link").Value);
+                yield return ai;
+            }
+        }
+    }
+}
---
+++ src/AllegroItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+
+namespace AllegroLINQ.src
+{
+    class AllegroItem
+    {
+        public string Title { get; set; }
+        public Uri ItemUri { get; set; }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("\nTitle - ");
+            sb.Append(Title);
+            sb.Append("\nItemUri - ");
+            sb.Append(ItemUri);
+            return sb.ToString();
+        }
+    }
+}
