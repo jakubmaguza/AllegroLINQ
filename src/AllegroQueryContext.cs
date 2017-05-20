d704d8fb52c768120a85b3624b9ea66dff78e6cc
Jakub Maguza <jakub.maguza@gmail.com>
2017-05-20 22:44:22 +0200

    Class with imitation of Execute method

---
+++ src/AllegroQueryContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq.Expressions;
+using System.Xml.Linq;
+
+namespace AllegroLINQ.src
+{
+    class AllegroQueryContext
+    {
+        public static object Execute(Expression expression)
+        {
+            return new List<AllegroItem>
+            {
+                new AllegroItem {Title = "C# cośtam", ItemUri = new Uri("http://www.c1.com/")},
+                new AllegroItem {Title = "C#", ItemUri = new Uri("http://www.c2.com/")}
+            };
+        }
+    }
+}
