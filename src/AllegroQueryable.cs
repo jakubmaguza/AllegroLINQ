f92a6ecf39bb76754e8c0182af2f53c671b14829
Jakub Maguza <jakub.maguza@gmail.com>
2017-05-20 22:41:31 +0200

    Class with implementation IQueryalbe<T> interface

---
+++ src/AllegroQueryable.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AllegroLINQ.src
+{
+    class AllegroQueryable<T> : IQueryable<T>
+    {
+        #region Constructors
+        /// <summary> 
+        /// This constructor is called by the client to create the data source. 
+        /// </summary> 
+        public AllegroQueryable()
+        {
+            Provider = new AllegroQueryProvider();
+            Expression = Expression.Constant(this);
+        }
+        /// <summary> 
+        /// This constructor is called by Provider.CreateQuery(). 
+        /// </summary> 
+        /// <param name="expression"></param>
+        public AllegroQueryable(AllegroQueryProvider provider, Expression expression)
+        {
+            if (provider == null)
+            {
+                throw new ArgumentNullException("provider");
+            }
+
+            if (expression == null)
+            {
+                throw new ArgumentNullException("expression");
+            }
+
+            if (!typeof(IQueryable<T>).IsAssignableFrom(expression.Type))
+            {
+                throw new ArgumentOutOfRangeException("expression");
+            }
+
+            Provider = provider;
+            Expression = expression;
+        }
+        #endregion Constructors
+        #region Enumerators
+        public IEnumerator<T> GetEnumerator()
+        {
+            return Provider.Execute<IEnumerable<T>>(Expression).GetEnumerator();
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return Provider.Execute<IEnumerable>(Expression).GetEnumerator();
+        }
+        #endregion Enumerators
+        #region Properties
+        public Expression Expression { get; private set; }
+        public Type ElementType => typeof(T);
+        public IQueryProvider Provider { get; private set; }
+        #endregion Properties
+    }
+}
