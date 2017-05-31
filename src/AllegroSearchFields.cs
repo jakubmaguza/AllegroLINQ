cfe2c435449f0eedc97bac94cdc4eda89452ad6d
Jakub Maguza <jakub.maguza@gmail.com>
2017-05-31 19:07:53 +0200

    Class with search criteria and BuildURL method

---
+++ src/AllegroSearchFields.cs
@@ -0,0 +1,19 @@
+﻿using System.Net;
+using System.Text;
+
+namespace AllegroLINQ.src
+{
+    class AllegroSearchFields
+    {
+        private static readonly string baseURL = "http://allegro.pl/rss.php/search?";
+        public string Title { get; set; }
+
+        public string BuildURL()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(baseURL);
+            sb.Append("string=").Append(WebUtility.UrlEncode(Title));
+            return sb.ToString();
+        }
+    }
+}
