133f719d2d784598e04eb3dfa3a55fc28fbea555
Jakub Maguza <jakub.maguza@gmail.com>
2017-05-31 19:09:11 +0200

    Expression tree translator class...
    Class that will accept Translate method and prepare AllegroSearchFileds criteria

---
+++ src/ExpressionTreeTranslator.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace AllegroLINQ.src
+{
+    class ExpressionTreeTranslator
+    {
+        private AllegroSearchFields _Criteria;
+        public string Translate(Expression expression)
+        {
+            _Criteria = new AllegroSearchFields();
+            Visit(expression);
+            return _Criteria.BuildURL();
+        }
+
+        private void Visit(Expression expression)
+        {
+            switch (expression.NodeType)
+            {
+                case ExpressionType.Call:
+                    VisitMethodCall((MethodCallExpression)expression);
+                    break;
+                case ExpressionType.Equal:
+                    VisitEqual((BinaryExpression)expression);
+                    break;
+                case ExpressionType.Lambda:
+                    Visit(((LambdaExpression)expression).Body);
+                    break;
+            }
+        }
+        private void VisitMethodCall(MethodCallExpression expression)
+        {
+            if ((expression.Method.DeclaringType == typeof(Queryable)) &&
+                (expression.Method.Name == "Where"))
+            {
+                Visit(((UnaryExpression)expression.Arguments[1]).Operand);
+            }
+            else
+            {
+                throw new NotSupportedException("Method not supported: " + expression.Method.Name);
+            }
+        }
+        private void VisitEqual(BinaryExpression expression)
+        {
+            if ((expression.Left.NodeType == ExpressionType.MemberAccess) &&
+                (((MemberExpression)expression.Left).Member.Name == "Title"))
+            {
+                if (expression.Right.NodeType == ExpressionType.Constant)
+                    _Criteria.Title = (String)((ConstantExpression)expression.Right).Value;
+                else if (expression.Right.NodeType == ExpressionType.MemberAccess)
+                    _Criteria.Title = (String)GetMemberValue((MemberExpression)expression.Right);
+                else
+                    throw new NotSupportedException("Expression type not supported for Title: " + expression.Right.NodeType);
+            }
+            else
+            {
+                throw new NotSupportedException("Cannot search by: " + ((MemberExpression)expression.Left).Member.Name);
+            }
+        }
+        private Object GetMemberValue(MemberExpression memberExpression)
+        {
+            MemberInfo memberInfo;
+            Object obj;
+
+            if (memberExpression == null)
+                throw new ArgumentNullException("memberExpression");
+
+            // Get object
+            if (memberExpression.Expression is ConstantExpression)
+                obj = ((ConstantExpression)memberExpression.Expression).Value;
+            else if (memberExpression.Expression is MemberExpression)
+                obj = GetMemberValue((MemberExpression)memberExpression.Expression);
+            else
+                throw new NotSupportedException("Expression type not supported: " + memberExpression.Expression.GetType().FullName);
+
+            // Get value
+            memberInfo = memberExpression.Member;
+            if (memberInfo is PropertyInfo)
+            {
+                PropertyInfo property = (PropertyInfo)memberInfo;
+                return property.GetValue(obj, null);
+            }
+            if (memberInfo is FieldInfo)
+            {
+                FieldInfo field = (FieldInfo)memberInfo;
+                return field.GetValue(obj);
+            }
+            throw new NotSupportedException("MemberInfo type not supported: " + memberInfo.GetType().FullName);
+
+        }
+    }
+}
